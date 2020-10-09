40b57bdd3cb6dc2a2ba9e35d3ace2920c6520a3b
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-09 15:24:08 +0530

    Update medicstore_management.c

--- 3_Implementation/src/medicstore_management.c
+++ 3_Implementation/src/medicstore_management.c
@@ -1,5 +1,14 @@
 #include <medicstore_management.h>
 
 int addentry(int operand1,int operand2)
-   {
-       
+{
+       return operand1 + operand2;
+}
+int display(int operand1,int operand2)
+{
+       return operand1 - operand2;
+}
+int deleteentry(int operand1,int operand2)
+{
+       return operand1 * operand2;
+}
