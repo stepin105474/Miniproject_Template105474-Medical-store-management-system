6820c283c2e852c2f9ac5eef468b048d67cda0cf
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-09 12:35:30 +0530

    Create medicstore_management.h

---
+++ 3_Implementation/inc/medicstore_management.h
@@ -0,0 +1,15 @@
+#ifndef __MEDICSTORE_MANAGEMENT_H__
+#define __MEDICSTORE_MANAGEMENT_H__
+
+#include <stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+
+void addentry();
+
+void display();
+
+void deleteentry();
+
+#endif  
