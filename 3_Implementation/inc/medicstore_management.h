08fd90693423a65698316a889c015310a6a1232a
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-09 15:16:22 +0530

    Update medicstore_management.h

--- 3_Implementation/inc/medicstore_management.h
+++ 3_Implementation/inc/medicstore_management.h
@@ -1,15 +1,15 @@
 #ifndef __MEDICSTORE_MANAGEMENT_H__
 #define __MEDICSTORE_MANAGEMENT_H__
 
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
 
-void addentry();
+int addentry(int operand1,int operand2);
 
-void display();
+int display(int operand1,int operand2);
 
-void deleteentry();
+int deleteentry(int operand1,int operand2);
 
 #endif  
