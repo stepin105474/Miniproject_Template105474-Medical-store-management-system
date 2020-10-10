10a0d0047e57f2d9165a5a6737eae7cfe2dae87a
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-10 19:01:09 +0530

    Update medicstore_management.h

--- 3_Implementation/inc/medicstore_management.h
+++ 3_Implementation/inc/medicstore_management.h
@@ -1,6 +1,12 @@
 #ifndef MEDICSTORE_MANAGEMENT_H
 #define MEDICSTORE_MANAGEMENT_H
 
+struct medicine
+{
+    int price,stock;
+    char name[50],mfg_date[12],exp_date[12];
+}m[100];
+
 int addentry(struct);
 
 int display(struct);
