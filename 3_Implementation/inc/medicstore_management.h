3afcf0a933e62132cd9350eeb31906fce3357db8
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-11 17:56:13 +0530

    Update medicstore_management.h

--- 3_Implementation/inc/medicstore_management.h
+++ 3_Implementation/inc/medicstore_management.h
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 struct medicine
 {
     int price,stock;
     char name[50],mfg_date[12],exp_date[12];
 }m[100];
+
 void addentry(struct medicine m[]);
 
 void display(struct medicine m[]);
