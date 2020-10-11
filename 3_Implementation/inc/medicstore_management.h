06540141218e11f974c972d0f16e526ca5fd8df9
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-11 17:13:43 +0530

    Update medicstore_management.h

--- 3_Implementation/inc/medicstore_management.h
+++ 3_Implementation/inc/medicstore_management.h
@@ -1,14 +1,14 @@
-#ifndef MEDICSTORE_MANAGEMENT_H
-#define MEDICSTORE_MANAGEMENT_H
+#ifndef __MEDICSTORE_MANAGEMENT_H__
+#define __MEDICSTORE_MANAGEMENT_H__
 
 #include <stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 
-void addentry(struct);
+void addentry(struct medicine m[]);
 
-void display(struct);
+void display(struct medicine m[]);
 
-void deleteentry(struct);
+void deleteentry(struct medicine m[]);
 
 #endif  
