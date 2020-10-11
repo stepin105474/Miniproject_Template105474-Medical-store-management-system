b11598e692b4ea910ec8a32757c28ffc76f33d77
stepin105474 <69641993+stepin105474@users.noreply.github.com>
2020-10-11 16:44:48 +0530

    Update medicstore_management.h

--- 3_Implementation/inc/medicstore_management.h
+++ 3_Implementation/inc/medicstore_management.h
@@ -1,6 +1,10 @@
 #ifndef MEDICSTORE_MANAGEMENT_H
 #define MEDICSTORE_MANAGEMENT_H
 
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
 void addentry(struct medicine m[]);
 
 void display(struct medicine m[]);
